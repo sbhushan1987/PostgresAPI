1ff67c1e559c898101d474989d4961a6c988c276
SHASHI BHUSHAN <bhushan1987@hotmail.com>
2018-09-04 20:12:45 +0530

    added swagger

--- DataSyncAPI/Startup.cs
+++ DataSyncAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DataSyncAPI
 {
@@ -25,6 +26,17 @@ namespace DataSyncAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Sellio POS APIs",
+                    Description = "Data sync apis for sellio app",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Sellio Team", Email = "contact@sellio.com", Url = "www.sellio.com" }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +48,11 @@ namespace DataSyncAPI
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sellio POS APIs");
+            });
         }
     }
 }
